85fa0757afc5ed47eed29fb68101df194503456c
elbeknurov <elbeknurov11@gmail.com>
2022-12-20 19:16:12 +0500

    loader

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const loader = document.querySelector(".loader");
+
+  setTimeout(() => {
+    loader.style.opacity;
+    loader.style.display = "none";
+  }, 9000);
+});
